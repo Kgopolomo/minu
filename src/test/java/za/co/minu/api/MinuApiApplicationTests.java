7d9bd0e187edd18e88e463c252e98f296ac2c700
Kgopolomo <Kgopolomo@gmail.com>
2023-07-06 14:37:30 +0200

    initial commit

---
+++ src/test/java/za/co/minu/api/MinuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.minu.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
